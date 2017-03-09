fd4c008ba304720859ae9225b7a515844f65d1b7
whyalone <whyalone0407@126.com>
2017-03-09 22:03:55 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -33,13 +33,12 @@ $(window).ready(function() {
 		}
 
 	})
-	
-	$("body").click(function(){
+
+	$("body").click(function() {
 		var menu_left = parseInt($("#nav .warp > ul").css("left"));
-		
+
 		$("#nav .warp > ul").css("left", -120);
-		
-		
+
 	})
 
 	function FnScroll() {
@@ -100,5 +99,40 @@ $(window).ready(function() {
 			fn && fn();
 		}, t);
 	}
+
+	$("#cooperater").mousedown(function(ev) {
+		ev.preventDefault();
+		var posx1 = parseInt(ev.clientX);
+		var distence = posx1 - parseInt($(".parrent_list").css("margin-left"));
+		var that = $(".parrent_list");
+		$(window).mousemove(function(ev2) {
+			var posx2 = parseInt(ev2.clientX);
+			var m_l = posx2 - distence;
+			if(m_l > 190) {
+				m_l = 190;
+			} else if(m_l < -750) {
+				m_l = -750;
+			} else {
+				that.css("transition", "none");
+			}
+			that.css("margin-left", m_l);
+
+			$(window).mouseup(function(ev) {
+				$(window).off("mousemove");
+				var fml = null;
+				if(m_l <= 190 && m_l > 0) {
+					fml = 0;
+					that.css("transition", "margin-left 0.5s");
+				} else if(m_l < -560 && m_l >= -750) {
+					fml = -560;
+					that.css("transition", "margin-left 0.5s");
+				} else {
+					fml = m_l;
+				}
+				that.css("margin-left", fml);
+			})
+
+		})
+	})
 
 })
